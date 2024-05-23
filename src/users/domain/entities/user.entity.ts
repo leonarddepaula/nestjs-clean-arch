b28a0509f0ec6d67605cc98daad71b232b5b591d
Leonardo de Paula <leodepaula315@gmail.com>
2024-05-23 10:32:59 -0300

    User entity -  Contructor unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createAt = props.createAt ?? new Date()
+  }
+}
