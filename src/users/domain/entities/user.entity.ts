39a85f69bc10b34b2cb6e9925e6521d4e9b336b7
Leonardo de Paula <leodepaula315@gmail.com>
2024-05-23 10:55:29 -0300

    UserEntity = getters methods unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,20 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createAt = props.createAt ?? new Date()
   }
+  get name(): string {
+    return this.props.name
+  }
+
+  get email(): string {
+    return this.props.email
+  }
+
+  get password(): string {
+    return this.props.password
+  }
+
+  get createAt(): Date {
+    return this.props.createAt
+  }
+
 }
