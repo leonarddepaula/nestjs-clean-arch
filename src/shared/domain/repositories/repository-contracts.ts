0acb11a436229e809935e337d6c697211f524401
Leonardo de Paula <leodepaula315@gmail.com>
2024-06-19 12:14:18 -0300

    refactor repository contracts

--- src/shared/domain/repositories/repository-contracts.ts
+++ src/shared/domain/repositories/repository-contracts.ts
@@ -1,9 +1,9 @@
-import { Entity } from "../entities/entity";
+import { Entity } from '../entities/entity'
 
 export interface RepositoryInterface<E extends Entity> {
-  inset (entity: E): Promise<void>;
-  update (entity: E): Promise<void>;
-  delete (entity: E): Promise<void>;
-  findById (id: string): Promise<E | null>;
-  findAll (): Promise<E[]>;
+  inset(entity: E): Promise<void>
+  findById(id: string): Promise<E | null>
+  findAll(): Promise<E[]>
+  update(entity: E): Promise<void>
+  delete(id: string): Promise<void>
 }
