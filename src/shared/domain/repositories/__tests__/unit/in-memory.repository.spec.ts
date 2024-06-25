128d1b26cecc142da9d199edd46926336a19856d
Leonardo de Paula <leodepaula315@gmail.com>
2024-06-25 19:39:32 -0300

    feat: add test in memory reposotiry

---
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -0,0 +1,36 @@
+import { Entity } from '@/shared/domain/entities/entity'
+import { InMemoryRepository } from '../../in-memory.repository'
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> {}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
+
+describe('InMemoryRepository unit tests', () => {
+  let sut: StubInMemoryRepository
+
+  beforeEach(() => {
+    sut = new StubInMemoryRepository()
+  })
+
+  it('Should inserts a new entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 })
+
+    await sut.inset(entity)
+
+    expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+
+
+  it('Should throw error when entity not found', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 })
+
+    await sut.inset(entity)
+
+    expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+})
