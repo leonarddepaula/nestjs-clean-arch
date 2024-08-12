dfde226747d099f3242b707b0cf38ee177c7ac0e
Leonardo de Paula <leodepaula315@gmail.com>
2024-08-12 18:40:19 -0300

    Unit tests searchResult

--- src/shared/domain/repositories/__tests__/unit/searchale-repository-contracts.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/searchale-repository-contracts.spec.ts
@@ -1,4 +1,7 @@
-import { SearchParams } from '../../searchale-repository-contracts'
+import {
+  SearchParams,
+  SearchResult,
+} from '../../searchale-repository-contracts'
 
 describe('SearchableRepositoryContracts', () => {
   describe('SearchParams tests', () => {
@@ -128,6 +131,72 @@ describe('SearchableRepositoryContracts', () => {
       params.forEach(i => {
         expect(new SearchParams({ filter: i.filter }).filter).toBe(i.expected)
       })
+    })
+  })
+
+  describe('Searchresult  tests', () => {
+    it('constructor props', () => {
+      let sut = new SearchResult({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: null,
+        sortDir: null,
+        filter: null,
+      })
+
+      sut = new SearchResult({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.toJSON()).toStrictEqual({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 2,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+
+      sut = new SearchResult({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 4,
+        currentPage: 1,
+        perPage: 10,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.lastPage).toBe(1)
+
+      sut = new SearchResult({
+        items: ['item1', 'item2', 'item3', 'item4'] as any,
+        total: 54,
+        currentPage: 1,
+        perPage: 10,
+        sort: 'name',
+        sortDir: 'asc',
+        filter: 'test',
+      })
+      expect(sut.lastPage).toBe(6)
     })
   })
 })
--- src/shared/domain/repositories/searchale-repository-contracts.ts
+++ src/shared/domain/repositories/searchale-repository-contracts.ts
@@ -17,7 +17,7 @@ export type SearchResultProps<E extends Entity, Filter> = {
   currentPage: number
   perPage: number
   sort: string | null
-  sortDirt: string | null
+  sortDir: string | null
   filter: Filter | null
 }
 
@@ -101,7 +101,7 @@ export class SearchResult<E extends Entity, Filter = string> {
   readonly total: number
   readonly currentPage: number
   readonly perPage: number
-  readonly lastPage
+  readonly lastPage: number
   readonly sort: string | null
   readonly sortDir: string | null
   readonly filter: Filter | null
@@ -113,7 +113,7 @@ export class SearchResult<E extends Entity, Filter = string> {
     this.perPage = props.perPage
     this.lastPage = Math.ceil(this.total / this.perPage)
     this.sort = props.sort ?? null
-    this.sortDir = props.sortDirt ?? null
+    this.sortDir = props.sortDir ?? null
     this.filter = props.filter ?? null
   }
 
@@ -133,8 +133,9 @@ export class SearchResult<E extends Entity, Filter = string> {
 
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
-  SearchOutput,
+  Filter = string,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
-  search(searchInput: SearchProps): Promise<SearchOutput>
+  search(props: SearchInput): Promise<SearchOutput>
 }
