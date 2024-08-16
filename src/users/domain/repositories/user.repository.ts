9b7cfaad09495e434447a410a8dc970628ecb99a
Leonardo de Paula <leodepaula315@gmail.com>
2024-08-16 18:40:26 -0300

    Refactoring user repository - part 1

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,24 @@
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchale-repository-contracts'
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchale-repository-contracts'
 import { UserEntity } from '../entities/user.entity'
 
-export interface UserRepository
-  extends SearchableRepositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>
-  emailExists(email: string): Promise<void>
+export namespace UserRepository {
+  export type Filter = string
+
+  export class SearchParams extends DefaultSearchParams {}
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> {}
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>
+    emailExists(email: string): Promise<void>
+  }
 }
