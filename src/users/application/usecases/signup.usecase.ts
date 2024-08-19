98bf68e1bf610409b18adce589aafc568b943b32
Leonardo de Paula <leodepaula315@gmail.com>
2024-08-19 14:11:03 -0300

    Adding BcryptjsHashProvider

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,3 +1,7 @@
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '../../domain/repositories/user.repository'
+import { BadRequestError } from '../errors/bad-request-error'
+
 export namespace SignUpUseCase {
   export type Input = {
     name: string
@@ -10,9 +14,24 @@ export namespace SignUpUseCase {
     name: string
     email: string
     password: string
-    creayedAt: Date
+    creayedAt?: Date
   }
   export class UseCase {
-    async execute(input: Input): Promise<Output> {}
+    constructor(private readonly userRepository: UserRepository.Repository) {}
+    async execute(input: Input): Promise<Output> {
+      const { name, email, password } = input
+
+      if (!name || !email || !password) {
+        throw new BadRequestError(
+          'Name, email and password are required - fill in all fields',
+        )
+      }
+      await this.userRepository.emailExists(email)
+
+      const entity = new UserEntity(input)
+
+      await this.userRepository.insert(entity)
+      return entity.toJSON()
+    }
   }
 }
