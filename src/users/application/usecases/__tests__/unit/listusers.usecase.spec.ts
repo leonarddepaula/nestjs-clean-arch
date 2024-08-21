3c7330cd22e52e9baa9bcb2e34a6ebde80d1971b
Leonardo de Paula <leodepaula315@gmail.com>
2024-08-21 16:38:22 -0300

    ListUserUseCase unit tests - part 2

--- src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
@@ -50,4 +50,50 @@ describe('ListUsers usecase unit tests', () => {
       perPage: result.perPage,
     })
   })
+
+  it('should return the users order by createdAt ', async () => {
+    const createdAt = new Date()
+    const items = [
+      new UserEntity(UserDataBuilder({ createdAt })),
+      new UserEntity(
+        UserDataBuilder({ createdAt: new Date(createdAt.getTime() + 1000) }),
+      ),
+    ]
+
+    repository.items = items
+    let output = await sut.execute({})
+    expect(output).toStrictEqual({
+      items: [...items].reverse().map(item => item.toJSON()),
+      total: 2,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 15,
+    })
+  })
+
+  it('should return the users pagination sort and filter ', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'a' })),
+      new UserEntity(UserDataBuilder({ name: 'AA' })),
+      new UserEntity(UserDataBuilder({ name: 'Aa' })),
+      new UserEntity(UserDataBuilder({ name: 'b' })),
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+    ]
+
+    repository.items = items
+    let output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'a',
+    })
+    expect(output).toStrictEqual({
+      items: [items[1].toJSON(), items[2].toJSON()],
+      total: 3,
+      currentPage: 1,
+      lastPage: 2,
+      perPage: 2,
+    })
+  })
 })
