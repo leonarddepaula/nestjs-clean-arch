c9415c3cf16290e62e9b00a3884e21ad12f34e99
Leonardo de Paula <leodepaula315@gmail.com>
2024-08-31 10:31:14 -0300

    update env-config

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from '@nestjs/common'
+import { SignupUseCase } from '../application/usecases/signup.usecase'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-momory.repository'
+import { BcryptjsHashProvider } from './hash-provider/bcryptjs-hash.provider'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useValue: BcryptjsHashProvider,
+    },
+    {
+      provide: SignupUseCase.UseCase,
+      useClass: SignupUseCase.UseCase,
+    },
+  ],
 })
 export class UsersModule {}
